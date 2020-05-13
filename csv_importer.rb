b1b52628a61ced34226d2d709a664fe3993b5e13
Victor Zaffalon <victor.zaffalon@udlab.com.br>
2020-05-13 20:43:40 -0300

    set up docker compose for redis and error handling

--- csv_importer.rb
+++ csv_importer.rb
@@ -10,21 +10,23 @@ require 'redis'
 @parsed_transactions_csv = nil
 
 class String
-    def is_number?
-        true if Integer(self) rescue false
-    end
+  def is_number?
+    true if Integer(self)
+  rescue StandardError
+    false
+  end
 end
 
 def receive_args
   if ARGV[0].nil? || ARGV[1].nil?
-    return puts '-- Error: Você precisa passar dois arquivos como parametro, o primeiro parâmetro deve ser um arquivo de contas e o segundo de transações'
+    raise StandardError.new('-- Error: Você precisa passar dois arquivos como parametro, o primeiro parâmetro deve ser um arquivo de contas e o segundo de transações')
   end
 end
 
 def create_redis_instance
   puts
   puts 'Instanciando redis..'
-  @redis = Redis.new
+  @redis = Redis.new(host: '127.0.0.1', port: 6380)
   @redis.flushall
 end
 
@@ -34,8 +36,8 @@ def open_csv_files
   begin
     @accounts_csv = File.read(ARGV[0])
     @transactions_csv = File.read(ARGV[1])
-  rescue => exception
-      return  puts '-- Error: Programa não conseguiu ler os arquivos!'
+  rescue StandardError => e
+    raise StandardError.new('-- Error: Programa não conseguiu ler os arquivos!')
   end
 end
 
@@ -43,11 +45,11 @@ def parse_csvs
   puts
   puts 'Fazendo o parse dos arquivos csv..'
   begin
-      # puts "accounts_csv" + accounts_csv
-      @parsed_accounts_csv = CSV.parse(@accounts_csv, headers: false)
-      @parsed_transactions_csv = CSV.parse(@transactions_csv, headers: false)
-  rescue => exception
-    return  puts '-- Error: Não conseguiu fazer o parse dos arquivos csv!'
+    # puts "accounts_csv" + accounts_csv
+    @parsed_accounts_csv = CSV.parse(@accounts_csv, headers: false)
+    @parsed_transactions_csv = CSV.parse(@transactions_csv, headers: false)
+  rescue StandardError => e
+    raise StandardError.new('-- Error: Não conseguiu fazer o parse dos arquivos csv!')
   end
 end
 
@@ -55,19 +57,19 @@ def create_accounts
   puts
   puts 'Criando contas..'
   @parsed_accounts_csv.each do |row|
-      account_id = row[0]
-      amount = row[1]
-      if account_id and account_id.is_number? and amount and amount.is_number?
-          account_exists = @redis.get(account_id)
-          if !account_exists
-            @redis.set(account_id, amount)
-          else
-            return  puts '-- Error: Csv mal formatado! Conta duplicada!'
-          end
+    account_id = row[0]
+    amount = row[1]
+    if account_id.is_number? && amount && amount.is_number?
+      account_exists = @redis.get(account_id)
+      if !account_exists
+        @redis.set(account_id, amount)
       else
-          return  puts '-- Error: Csv mal formatado! Colunas devem ser números inteiros!'
+        raise StandardError.new("-- Error: Csv mal formatado! Conta (#{account_id}) duplicada!")
       end
-  end 
+    else
+      raise StandardError.new('-- Error: Csv mal formatado! Colunas devem ser números inteiros!')
+    end
+  end
 end
 
 def create_transactions
@@ -76,15 +78,15 @@ def create_transactions
   @parsed_transactions_csv.each do |row|
     account_id = row[0]
     amount = row[1]
-    if account_id and account_id.is_number? and amount and amount.is_number?
+    if account_id.is_number? && amount && amount.is_number?
       account_exists = @redis.get(account_id)
       if account_exists
         @redis.set(account_id, amount)
       else
-        return  puts '-- Error: Csv mal formatado! Colunas devem ser números inteiros!'
+       raise StandardError.new("-- Error: Csv mal formatado! Conta (#{account_id}) não existente!")
       end
     else
-      return  puts '-- Error: Csv mal formatado! Colunas devem ser números inteiros!'
+      raise StandardError.new('-- Error: Csv mal formatado! Colunas devem ser números inteiros!')
     end
     amount = amount.to_i
     current_amount = @redis.get(account_id).to_i
@@ -98,18 +100,21 @@ def output_result
   puts
   puts 'Resultado das transações:'
   puts
-  @redis.keys.each do |key,value|
+  @redis.keys.each do |key, _value|
     puts 'id da conta:' + key
-    puts "saldo final:" + @redis.get(key)
+    puts 'saldo final:' + @redis.get(key)
     puts
   end
 end
 
-receive_args()
-create_redis_instance()
-open_csv_files()
-parse_csvs()
-create_accounts()
-create_transactions()
-output_result()
-
+begin
+  receive_args
+  create_redis_instance
+  open_csv_files
+  parse_csvs
+  create_accounts
+  create_transactions
+  output_result
+rescue StandardError => e
+  puts e
+end
